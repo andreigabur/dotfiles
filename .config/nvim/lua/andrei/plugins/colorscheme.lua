c460b7edc089fa26cc4dfe5275c3d5ad2dc6a1ec
Andrei Gabur <andrei@rootline.com>
2025-03-11 23:03:05 +0100

    Wezterm

--- .config/nvim/lua/andrei/plugins/colorscheme.lua
+++ .config/nvim/lua/andrei/plugins/colorscheme.lua
@@ -1,24 +1,23 @@
 return {
   {
-    "catppuccin/nvim", 
-    name = "catppuccin", 
+    "catppuccin/nvim",
+    lazy = false,
     priority = 1000,
     config = function()
       require("catppuccin").setup({
         -- transparent_background = true,
       })
-      -- vim.cmd("colorscheme catppuccin-latte")
+      vim.cmd("colorscheme catppuccin-macchiato")
     end,
   },
   {
     "Mofiqul/dracula.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("dracula").setup({
-        transparent_bg = true,
-      })
-      vim.cmd("colorscheme dracula")
-    end,
   },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+  }
 }
--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Catppuccin Frappe"
 
 -- config.background = {{
 --     source = {
@@ -22,7 +22,7 @@ config.color_scheme = "Catppuccin Mocha"
 --     opacity = 0.9
 -- }}
 
--- config.window_background_opacity = 0.8
+config.window_background_opacity = 0.8
 
 config.tab_max_width = 100
 
@@ -30,18 +30,13 @@ config.use_fancy_tab_bar = false
 
 config.show_tab_index_in_tab_bar = true
 
-config.colors = {
-    background = "rgba(24, 24, 37, 0.7)",
-    tab_bar = {
-        background = "rgba(24, 24, 37, 0.7)"
-    }
-}
-
 config.font = wezterm.font 'Hack Nerd Font'
 
 config.font_size = 12.0
 
-config.window_decorations = "RESIZE"
+config.window_decorations = "TITLE | RESIZE"
+
+config.tab_max_width = 30
 
 config.window_padding = {
     left = 20,
@@ -52,8 +47,6 @@ config.window_padding = {
 
 config.initial_cols = 200
 config.initial_rows = 60
-
-config.xcursor_theme = "Yaru"
 
 config.keys = {
     {
