0f4cd97839dd15e6f511f34cfaa97099f24463d3
Andrei Gabur <andrei@rootline.com>
2025-03-16 22:47:55 +0100

    Add waveterm

--- .config/nvim/lua/andrei/plugins/colorscheme.lua
+++ .config/nvim/lua/andrei/plugins/colorscheme.lua
@@ -7,13 +7,16 @@ return {
       require("catppuccin").setup({
         -- transparent_background = true,
       })
-      vim.cmd("colorscheme catppuccin-macchiato")
+      -- vim.cmd("colorscheme catppuccin-macchiato")
     end,
   },
   {
     "Mofiqul/dracula.nvim",
     lazy = false,
     priority = 1000,
+    config = function()
+      vim.cmd("colorscheme dracula")
+    end,
   },
   {
     "folke/tokyonight.nvim",
